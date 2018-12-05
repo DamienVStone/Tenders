cb9981ab992c05b223403db3799f881734098ba9
ssds-team <Dmitry.Nikitin@ingos.ru>
2018-12-05 12:57:31 +0300

    new project

---
+++ DELETEME/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DELETEME
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
