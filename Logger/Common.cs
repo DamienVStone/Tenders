c4bc64c1ade9678ec51c7c41e470c56774f9f376
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2018-12-07 15:16:37 +0300

    Добавил исходники проекта .NET Framework в проект .NET Core

---
+++ Logger/Common.cs
@@ -0,0 +1,37 @@
+﻿using AppLogger.Configuration;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppLogger
+{
+    public static class Common
+    {
+        private static string _apiUrl;
+
+        static Common()
+        {
+            var section = (SSDSApiLoggerSection)ConfigurationManager.GetSection("SSDSApiLogger");
+            if (section == null) throw new InvalidOperationException("Не могу найти секцию SSDSApiLogger в файле конфигурации");
+
+            _apiUrl = section.ApiUrl;
+        }
+
+        public static void ClearOutdated()
+        {
+            using (var client = new HttpClient())
+            {
+                var response = client.PostAsync(_apiUrl+"/api/clear", null).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new InvalidOperationException("Не удалось очистить устревшие статусы. Ответ: "+response.StatusCode);
+                }
+            }
+        }
+
+    }
+}
---
+++ Logger/LogCache.cs
@@ -0,0 +1,51 @@
+﻿using AppLogger.Models;
+using AppLogger.Utils;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Timers;
+
+namespace AppLogger
+{
+    class LogCache
+    {
+        readonly object _cacheLock = new object();
+        List<LogEntry> _cache = new List<LogEntry>();
+
+        public LogCache(Action<List<string>> SendAction, long TimerInterval)
+        {
+            var timer = new Timer();
+            timer.Interval = TimerInterval;
+            timer.Elapsed += (s, e) =>
+            {
+                timer.Stop();
+                List<string> serializedData = null;
+                lock (_cacheLock)
+                {
+                    if (_cache.Count != 0)
+                    {
+                        serializedData = _cache
+                            .SplitIntoBatches(5000)
+                            .Select(b => JsonConvert.SerializeObject(s))
+                            .ToList();
+                        _cache.Clear();
+                    }
+                }
+
+                SendAction(serializedData ?? new List<string>());
+                timer.Start();
+            };
+            timer.Start();
+        }
+
+        public void Add(LogEntry Value)
+        {
+            lock (_cacheLock)
+            {
+                _cache.Add(Value);
+            }
+        }
+
+    }
+}
