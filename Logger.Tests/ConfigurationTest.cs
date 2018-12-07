1d7179f0b63c9c33d2c74977ad3a5408521193c1
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2018-12-07 18:30:05 +0300

    Добавил тест для новой конфигурации

---
+++ Logger.Tests/ConfigurationTest.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Logger.Tests
+{
+    [TestClass]
+    public class ConfigurationTest
+    {
+        [TestMethod]
+        public void LoggerCreationTest() {
+            var logger = AppLogger.Logger.GetLogger("Profiler1");
+            Assert.IsNotNull(logger);
+        }
+    }
+}
