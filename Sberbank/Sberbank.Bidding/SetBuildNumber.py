97e21695a4e4295b9e47acb3f00688c1d0ab764a
ssds-team <Dmitry.Nikitin@ingos.ru>
2018-12-12 15:04:12 +0300

    build number setting fix

--- Sberbank/Sberbank.Bidding/SetBuildNumber.py
+++ Sberbank/Sberbank.Bidding/SetBuildNumber.py
@@ -1,11 +1,10 @@
-filePath, buildNumber = sys.argv[1:3]
+import sys
 
+filePath, buildNumber = sys.argv[1:3]
 f = open(filePath,'r')
 filedata = f.read()
 f.close()
-
 newdata = filedata.replace("{{BUILDNUMBER}}", buildNumber)
-
 f = open(filePath,'w')
 f.write(newdata)
 f.close()
