1025271e38a56d4a70e3a20f98256bd24da014c1
DamienVStone <Dmitry_Nikitin@iCloud.com>
2018-12-27 10:49:28 +0300

    more debug infos

--- Integration/API/Tenders.Integration.API.JobSeeker/Services/JobSeekerConfigService.cs
+++ Integration/API/Tenders.Integration.API.JobSeeker/Services/JobSeekerConfigService.cs
@@ -1,9 +1,6 @@
 ﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
 using System.Web;
 using Tenders.Core.Abstractions.Services;
-using Tenders.Core.Helpers;
 using Tenders.Integration.API.JobSeeker.Interfaces;
 using Tenders.Sberbank.Abstractions.Models;
 
@@ -32,41 +29,5 @@ namespace Tenders.Integration.API.JobSeeker.Services
             return result;
         }
 
-    }
-
-    public class K8sConfigMapsConfigService : IConfigService
-    {
-        // 
-        private object o = new object();
-        private Dictionary<string, string> _configs;
-
-        public string this[string index]
-        {
-            get
-            {
-                if (_configs == null)
-                    lock (o)
-                        if (_configs == null)
-                            _configs = new Dictionary<string, string>();
-
-                if (!_configs.ContainsKey(index))
-                    lock (o)
-                        if (!_configs.ContainsKey(index))
-                        {
-                            Console.WriteLine("I am is " + ("whoami".Bash()));
-                            var result = @"sudo kubectl get cm api -n tenders -o jsonpath='{.data.api\.TokenUrl}'".Bash()?.Trim();
-                            if (string.IsNullOrEmpty(result))
-                                result = @"sudo kubectl get cm sberbank -n tenders -o jsonpath='{.data.api\.TokenUrl}'".Bash()?.Trim();
-                            if (string.IsNullOrEmpty(result))
-                                result = @"sudo kubectl get cm jobseeker -n tenders -o jsonpath='{.data.api\.TokenUrl}'".Bash()?.Trim();
-                            if (string.IsNullOrEmpty(result))
-                                throw new KeyNotFoundException(index);
-                            Console.WriteLine("config value got: " + result);
-                            _configs[index] = result;
-                        }
-
-                return _configs[index];
-            }
-        }
     }
 }
---
+++ Integration/API/Tenders.Integration.API.JobSeeker/Services/K8sConfigMapsConfigService.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using Tenders.Core.Abstractions.Services;
+using Tenders.Core.Helpers;
+
+namespace Tenders.Integration.API.JobSeeker.Services
+{
+    public class K8sConfigMapsConfigService : IConfigService
+    {
+        // 
+        private object o = new object();
+        private Dictionary<string, string> _configs;
+
+        public string this[string index]
+        {
+            get
+            {
+                Console.WriteLine("I am is " + ("whoami".Bash()));
+                Console.WriteLine("try to get " + index);
+                if (_configs == null)
+                    lock (o)
+                        if (_configs == null)
+                            _configs = new Dictionary<string, string>();
+
+                if (!_configs.ContainsKey(index))
+                    lock (o)
+                        if (!_configs.ContainsKey(index))
+                        {
+                            var result = @"sudo kubectl get cm api -n tenders -o jsonpath='{.data.api\.TokenUrl}'".Bash()?.Trim();
+                            if (string.IsNullOrEmpty(result))
+                                result = @"sudo kubectl get cm sberbank -n tenders -o jsonpath='{.data.api\.TokenUrl}'".Bash()?.Trim();
+                            if (string.IsNullOrEmpty(result))
+                                result = @"sudo kubectl get cm jobseeker -n tenders -o jsonpath='{.data.api\.TokenUrl}'".Bash()?.Trim();
+                            if (string.IsNullOrEmpty(result))
+                                throw new KeyNotFoundException(index);
+                            _configs[index] = result;
+                        }
+
+                Console.WriteLine("config value got: " + _configs[index]);
+                return _configs[index];
+            }
+        }
+    }
+}
