bd1dcde4d852966b385a1f7892ccf30b5da1e387
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2018-12-29 14:09:00 +0300

    Разбил конфиги подключнеия к базе данных на несколько пунктов

--- API/Tenders.API/Services/APIConfigService.cs
+++ API/Tenders.API/Services/APIConfigService.cs
@@ -1,10 +1,12 @@
-﻿using Tenders.Core.Abstractions.Services;
+﻿using System.Collections.Generic;
+using Tenders.Core.Abstractions.Services;
 
 namespace Tenders.API.Services
 {
     public interface IAPIConfigService
     {
         string DbConnectionString { get; }
+        string DbName { get; }
         /// <summary>
         /// Количество часов через которое путь на FTP должен быть автоматически проиндексирован. Используется в PathService
         /// </summary>
@@ -17,11 +19,33 @@ namespace Tenders.API.Services
 
         public int FTPIndexingTimeout { get; private set; }
 
+        public string DbName { get; private set; }
+
         public APIConfigService(IConfigService config)
         {
             if (config == null) throw new System.ArgumentNullException(nameof(config));
 
-            DbConnectionString = config["api.DbConnectionString"];
+            string creds = "";
+
+            try
+            {
+                var username = config["api.DbUsername"];
+                if (!string.IsNullOrWhiteSpace(username))
+                {
+                    creds += username;
+                    try
+                    {
+                        var password = config["api.DbPassword"];
+                        if (!string.IsNullOrWhiteSpace(password)) creds += ":" + password;
+                    }
+                    catch (KeyNotFoundException) { }
+                    creds += "@";
+                }
+            }
+            catch (KeyNotFoundException) {}
+
+            DbConnectionString = $"{config["api.DbConnectionScheme"]}://{creds}{config["api.DbHost"]}:{config["api.DbPort"]}/";
+            DbName = config["api.DbName"];
             FTPIndexingTimeout = int.Parse(config["api.FTPIndexingTimeout"]);
         }
 
