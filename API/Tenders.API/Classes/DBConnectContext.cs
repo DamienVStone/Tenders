72ddd95dd08b13d3583ff2cfd61365a7257fbca5
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2018-12-29 12:31:39 +0300

    Сделал DbConnectContext синглтоном

--- API/Tenders.API/Classes/DBConnectContext.cs
+++ API/Tenders.API/Classes/DBConnectContext.cs
@@ -23,48 +23,72 @@ namespace TenderPlanAPI.Controllers
             this.config = config ?? throw new System.ArgumentNullException(nameof(config));
         }
 
-        public IMongoCollection<Customer> Customers => _connectionTenderPlan<Customer>("customers");
-        public IMongoCollection<TenderPlan> TenderPlans => _connectionTenderPlan<TenderPlan>("tenderPlans");
-        public IMongoCollection<TenderPlanPosition> TenderPlanPositions => _connectionTenderPlan<TenderPlanPosition>("tenderPlanPosition");
+        public IMongoCollection<Customer> Customers => new TransientDBContext(config).Customers;
 
-        public IMongoCollection<TenderPlanIndex> TenderPlansIndex
+        public IMongoCollection<TenderPlan> TenderPlans => new TransientDBContext(config).TenderPlans;
+
+        public IMongoCollection<TenderPlanPosition> TenderPlanPositions => new TransientDBContext(config).TenderPlanPositions;
+
+        public IMongoCollection<TenderPlanIndex> TenderPlansIndex => new TransientDBContext(config).TenderPlansIndex;
+
+        public IMongoCollection<FTPPath> FTPPath => new TransientDBContext(config).FTPPath;
+
+        public IMongoCollection<FTPEntry> FTPEntry => new TransientDBContext(config).FTPEntry;
+
+        private class TransientDBContext
         {
-            get
+            private readonly IAPIConfigService config;
+
+            public TransientDBContext(IAPIConfigService config)
             {
-                var tpInd = _connectionTenderPlan<TenderPlanIndex>("tenderPlansIndex");
-                var crtInd = Builders<TenderPlanIndex>.IndexKeys.Ascending(tpi => tpi.TenderPlanId);
-                var crtIndOpt = new CreateIndexOptions<TenderPlanIndex> { Unique = true };
-                var crtIndModel = new CreateIndexModel<TenderPlanIndex>(crtInd, crtIndOpt);
-                tpInd.Indexes.CreateOne(crtIndModel);
-                return tpInd;
+                this.config = config ?? throw new System.ArgumentNullException(nameof(config));
             }
-        }
 
-        public IMongoCollection<FTPPath> FTPPath => _connectionFTPMonitor<FTPPath>("ftpPath");
+            public IMongoCollection<Customer> Customers => _connectionTenderPlan<Customer>("customers");
+            public IMongoCollection<TenderPlan> TenderPlans => _connectionTenderPlan<TenderPlan>("tenderPlans");
+            public IMongoCollection<TenderPlanPosition> TenderPlanPositions => _connectionTenderPlan<TenderPlanPosition>("tenderPlanPosition");
 
-        public IMongoCollection<FTPEntry> FTPEntry => _connectionFTPMonitor<FTPEntry>("ftpFile");
-
-        private MongoClient client
-        {
-            get
+            public IMongoCollection<TenderPlanIndex> TenderPlansIndex
             {
-                return new MongoClient(config.DbConnectionString);
+                get
+                {
+                    var tpInd = _connectionTenderPlan<TenderPlanIndex>("tenderPlansIndex");
+                    var crtInd = Builders<TenderPlanIndex>.IndexKeys.Ascending(tpi => tpi.TenderPlanId);
+                    var crtIndOpt = new CreateIndexOptions<TenderPlanIndex> { Unique = true };
+                    var crtIndModel = new CreateIndexModel<TenderPlanIndex>(crtInd, crtIndOpt);
+                    tpInd.Indexes.CreateOne(crtIndModel);
+                    return tpInd;
+                }
             }
-        }
+
+            public IMongoCollection<FTPPath> FTPPath => _connectionFTPMonitor<FTPPath>("ftpPath");
+
+            public IMongoCollection<FTPEntry> FTPEntry => _connectionFTPMonitor<FTPEntry>("ftpFile");
+
+            private MongoClient client
+            {
+                get
+                {
+                    return new MongoClient(config.DbConnectionString);
+                }
+            }
 
 
-        private IMongoCollection<T> _connectionTenderPlan<T>(string name)
-        {
-            var db = client.GetDatabase("TenderPlans");
-            return db.GetCollection<T>(name);
-        }
+            private IMongoCollection<T> _connectionTenderPlan<T>(string name)
+            {
+                var db = client.GetDatabase("TenderPlans");
+                return db.GetCollection<T>(name);
+            }
 
-        private IMongoCollection<T> _connectionFTPMonitor<T>(string name)
-        {
-            var dbName = "FTPMonitor";
-            var db = client.GetDatabase(dbName);
-            return db.GetCollection<T>(name);
+            private IMongoCollection<T> _connectionFTPMonitor<T>(string name)
+            {
+                var dbName = "FTPMonitor";
+                var db = client.GetDatabase(dbName);
+                return db.GetCollection<T>(name);
+            }
         }
 
     }
+
+
 }
