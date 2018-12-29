965d04080a647d1bc22286ec4e03713ba389e38a
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2018-12-29 17:03:44 +0300

    Исправил мелкие ошибки после рефокторинга

--- Synchronization/Tenders.Synchronization.FtpMonitoring/Program.cs
+++ Synchronization/Tenders.Synchronization.FtpMonitoring/Program.cs
@@ -2,8 +2,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Net.Http;
+using System.Net.Mime;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 using Tenders.Core.Abstractions.Services;
@@ -33,14 +36,19 @@ namespace FtpMonitoringService
             if (p == null) return;
 
             List<FtpFile> allEntries = new List<FtpFile>();
-            //#if DEBUG
-            //            var creds = File.ReadAllLines("creds.txt");
-            //            p.Login = creds[0];
-            //            p.Password = creds[1];
-            //#endif
+#if DEBUG
+            var creds = File.ReadAllLines("creds.txt");
+            p.Login = creds[0];
+            p.Password = creds[1];
+#endif
             var files = FtpClient.Get().ListDirectoryFiels(p.Path, p.Login, p.Password);
-            var content = new StringContent(JsonConvert.SerializeObject(files.Where(f => !f.Name.EndsWith(".zip")).ToList()));
-            apiDataProvider.SendFilesAsync(content, p.Id, ct).Wait();
+            var notZipFilesToSend = files.Where(f => !f.Name.EndsWith(".zip")).ToList();
+            if(notZipFilesToSend.Count != 0)
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(notZipFilesToSend), Encoding.UTF8, MediaTypeNames.Application.Json);
+                apiDataProvider.SendFilesAsync(content, p.Id, ct).Wait();
+            }
+            
 
             files
                 .Where(f => f.Name.EndsWith(".zip"))
@@ -49,7 +57,7 @@ namespace FtpMonitoringService
                 {
                     var fs = ZipHelper.Get().ParseArchve(FtpClient.Get().GetArchiveEntries(p.Path + f.Name, p.Login, p.Password));
                     fs.ForEach(file => file.Parent = f.Id);
-                    var res = apiDataProvider.SendFileTreeAsync(new StringContent(JsonConvert.SerializeObject(fs)), ct).Result;
+                    var res = apiDataProvider.SendFileTreeAsync(new StringContent(JsonConvert.SerializeObject(new { PathId = p.Id, TreeRoot = f, Files = fs }), Encoding.UTF8, MediaTypeNames.Application.Json), ct).Result;
                     logger.Log(res);
                 });
         }
