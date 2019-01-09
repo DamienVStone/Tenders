4d150f977f4574845036f560aa23c3d093165846
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2019-01-09 16:52:52 +0300

    Добавил тест, отправляющий тестовые данные о путях в боевое АПИ

---
+++ Tenders.API.Tests/MonitoringUnit.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+
+namespace Tenders.API.Tests
+{
+    public class MonitoringUnit
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+        public bool enabled { get; set; }
+        public bool isNeedIndex { get; set; }
+        public List<string> paths { get; set; }
+        public List<string> tempPaths { get; set; }
+    }
+}
