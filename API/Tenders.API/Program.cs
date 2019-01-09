ce81732a0056af965d5bbae0256a490f2d77d9f2
DamienVStone <Dmitry_Nikitin@iCloud.com>
2019-01-09 11:34:58 +0300

    expose 5000 port

--- API/Tenders.API/Program.cs
+++ API/Tenders.API/Program.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore;
+﻿using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Logging;
 
 namespace TenderPlanAPI
 {
@@ -19,7 +12,7 @@ namespace TenderPlanAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseUrls("http://localhost:5000/")
+                .UseUrls("http://0.0.0.0:5000")
                 .UseKestrel()
                 .UseStartup<Startup>();
     }
