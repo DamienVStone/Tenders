db1108957bbe9f772c36bb426e76ae770f4936f3
DamienVStone <Dmitry_Nikitin@iCloud.com>
2019-01-14 17:55:48 +0300

    added material + created main template

--- UI/src/app/app-routing.module.ts
+++ UI/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MonitoringComponent } from './components/monitoring/monitoring.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'monitoring', component: MonitoringComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
