5c73c9c39eb7e1e462d523ce6ca572ad9f031b14
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2019-01-14 12:56:29 +0300

    Переписал сервис TreeLooker

--- API/Tenders.API/Services/Interfaces/ITreeLookerService.cs
+++ API/Tenders.API/Services/Interfaces/ITreeLookerService.cs
@@ -1,7 +1,11 @@
-﻿namespace Tenders.API.Services.Interfaces
+﻿using System.Collections.Generic;
+using TenderPlanAPI.Models;
+using TenderPlanAPI.Parameters;
+
+namespace Tenders.API.Services.Interfaces
 {
     public interface ITreeLookerService
     {
-        void UpdateFiles(string DbParentId, string InputParentId);
+        void UpdateFiles(string PathId, ISet<FTPEntry> DbFiles, ISet<FTPEntryParam> InputFiles, string DbParentId, string InputParentId);
     }
 }
--- API/Tenders.API/Services/TreeLooker.cs
+++ API/Tenders.API/Services/TreeLooker.cs
@@ -1,115 +1,83 @@
-﻿using MongoDB.Bson;
-using MongoDB.Driver;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using TenderPlanAPI.Controllers;
 using TenderPlanAPI.Enums;
 using TenderPlanAPI.Models;
 using TenderPlanAPI.Parameters;
+using Tenders.API.DAL.Interfaces;
 using Tenders.API.Services.Interfaces;
 
 namespace TenderPlanAPI.Services
 {
-    public class TreeLookerService: ITreeLookerService
+    public class TreeLookerService : ITreeLookerService
     {
-        private ObjectId _pathId; //идентификатор FTP пути по которому лежит дерево
-        private List<FTPEntry> _dbFiles; //список файлов, полученных из базы данных
-        private List<FTPEntryParam> _inputFiles; //список файлов, полученных из тела запроса
-        private readonly IDBConnectContext dbContext;
+        private readonly IFTPEntryRepo _entryRepo;
+        private readonly IFTPPathRepo _pathRepo;
 
-        /// <summary>
-        /// Класс, рекурсивно обходящий и индексирующий деревья файлов
-        /// </summary>
-        /// <param name="PathId">идентификатор FTP пути по которому лежит дерево</param>
-        /// <param name="DbFiles">список файлов, полученных из базы данных</param>
-        /// <param name="InputFiles">список файлов, полученных из тела запроса</param>
-        public TreeLookerService(ObjectId PathId, List<FTPEntry> DbFiles, List<FTPEntryParam> InputFiles, IDBConnectContext DbContext)
+        public TreeLookerService(IFTPEntryRepo entryRepo, IFTPPathRepo pathRepo)
         {
-            _pathId = PathId;
-            _dbFiles = DbFiles;
-            _inputFiles = InputFiles;
-            dbContext = DbContext ?? throw new ArgumentNullException(nameof(DbContext));
-            _dbFiles.AsParallel().ForAll(f => f.State = StateFile.Pending);
+            _entryRepo = entryRepo ?? throw new ArgumentNullException(nameof(entryRepo));
+            _pathRepo = pathRepo ?? throw new ArgumentNullException(nameof(pathRepo));
         }
 
-        /// <summary>
-        /// Функция, обновляющая все файлы внутри очередной папки. Вызывается рекурсивно.
-        /// </summary>
-        /// <param name="dbParentId">Идентификатор папки базы данных внутри которой проиндексируются файлы</param>
-        /// <param name="inputParentId">Идентификатор папки на входящем дереве с файлами из которой будет происходить сравнение</param>
-        public void UpdateFiles(string dbParentId, string inputParentId)
+        public void UpdateFiles(string PathId, ISet<FTPEntry> DbFiles, ISet<FTPEntryParam> InputFiles, string DbParentId, string InputParentId)
         {
+            var pathId = Guid.Parse(PathId);
+            var dbParentId = Guid.Parse(DbParentId);
+            var inputParentId = Guid.Parse(InputParentId);
+
             var key = new object();
-            var dbFolderContents = _dbFiles.Where(f => f.Parent.Equals(dbParentId)).ToDictionary(f => f.Name);
-            var inputFolderContents = _inputFiles.Where(f => f.Parent.Equals(inputParentId)).ToList();
+            var dbFolderContents = DbFiles.Where(f => f.Parent.Equals(dbParentId)).ToDictionary(f => f.Name);
+            var inputFolderContents = InputFiles.Where(f => f.Parent.Equals(inputParentId));
 
             inputFolderContents
                 .AsParallel()
                 .ForAll(f =>
                 {
+                    FTPEntry dbFile;
+
                     if (dbFolderContents.Keys.Contains(f.Name))
                     {
                         //Файл существует в базе
-                        var dbFile = dbFolderContents[f.Name];
-
-                        var updates = new List<UpdateDefinition<FTPEntry>>();
-                        var isFileModified = false;
-
-                        if (dbFile.Size != f.Size)
-                        {
-                            updates.Add(Builders<FTPEntry>.Update.Set("Size", f.Size));
-                            isFileModified = true;
-                        }
-                        if (!dbFile.Modified.Equals(f.DateModified))
-                        {
-                            updates.Add(Builders<FTPEntry>.Update.Set("Modified", f.DateModified));
-                            isFileModified = true;
-                        }
-
-                        var filter = Builders<FTPEntry>.Filter.Eq("_id", dbFile.Id);
-                        if (isFileModified)
+                        dbFile = dbFolderContents[f.Name];
+                        if (dbFile.Size != f.Size || !dbFile.Modified.Equals(f.DateModified))
                         {
                             lock (key)
                             {
-                                dbFolderContents[f.Name].State = StateFile.Modified;
+                                dbFile.Modified = f.DateModified;
+                                dbFile.Size = f.Size;
+                                dbFile.State = StateFile.Modified;
                             }
-                            updates.Add(Builders<FTPEntry>.Update.Set("State", StateFile.Modified));
-                            dbContext.FTPEntry.UpdateOne(filter, Builders<FTPEntry>.Update.Combine(updates));
+                            _entryRepo.Update(dbFile);
                         }
                         else
                         {
                             lock (key)
                             {
-                                dbFolderContents[f.Name].State = StateFile.Indexed;
+                                dbFile.State = StateFile.Indexed;
                             }
                         }
-
-                        // Вызываю ту же функцию для данного файла, чтобы проиндексироваьт всех детей
-                        UpdateFiles(dbFile.Id, f.Id);
                     }
                     else
                     {
                         //Файл не существует в базе
-                        var newFile = new FTPEntry
+                        dbFile = new FTPEntry
                         {
                             Name = f.Name,
                             Modified = f.DateModified,
                             Size = f.Size,
-                            Path = _pathId,
+                            Path = pathId,
                             Parent = dbParentId
                         };
 
-                        dbContext.FTPEntry.InsertOne(newFile);
-
-                        // Вызываю ту же функцию для данного файла, чтобы проиндексироваьт всех детей
-                        UpdateFiles(newFile.Id, f.Id);
+                        dbFile.Id = Guid.Parse(_entryRepo.Create(dbFile));
                     }
+                    // Вызываю ту же функцию для данного файла, чтобы проиндексироваьт всех детей
+                    UpdateFiles(PathId, DbFiles, InputFiles, dbFile.Id.ToString(), f.Id.ToString());
 
                 });
 
-            var forDelete = dbFolderContents.Values.Where(f => f.State == StateFile.Pending).Select(f => Builders<FTPEntry>.Filter.Eq("_id", f.Id)).ToArray();
-            if (forDelete.Length > 0) dbContext.FTPEntry.DeleteMany(Builders<FTPEntry>.Filter.Or(forDelete));
+            dbFolderContents.Values.Where(f => f.State == StateFile.Pending).Select(f => f.Id.ToString()).AsParallel().ForAll(e => _entryRepo.Delete(e));
         }
     }
 }
