21eb4cd0e0bde1d24138d1c7d74bcaf307ae6b1a
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2019-01-15 16:34:56 +0300

    Создал базовый класс репозитория Mongo

--- API/Tenders.API/DAL/Mongo/BaseMongoRepo.cs
+++ API/Tenders.API/DAL/Mongo/BaseMongoRepo.cs
@@ -1,49 +1,78 @@
-﻿using System.Collections.Generic;
+﻿using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
 using TenderPlanAPI.Models;
 using Tenders.API.DAL.Interfaces;
 
 namespace Tenders.API.DAL.Mongo
 {
-    public class BaseMongoRepo<T> : IAPIRepository<T> where T : ModelBase
+    public abstract class BaseMongoRepo<T> : IAPIRepository<T> where T : ModelBase
     {
+        protected IIdProvider IdProvider;
+
+        public BaseMongoRepo(IIdProvider idProvider)
+        {
+            IdProvider = idProvider;
+        }
+
+        protected abstract IMongoCollection<T> Entities { get; }
+
+        protected abstract UpdateDefinition<T> createUpdateDefinition(T db, T inp);
+
         public bool ChangeActiveFlag(string Id, bool IsActive)
         {
-            throw new System.NotImplementedException();
+            checkId(Id);
+            var update = Builders<T>.Update.Set(f => f.IsActive, IsActive);
+            return Entities.FindOneAndUpdate(f => f.Id == Id, update) != null;
         }
 
         public long CountAll(bool IsActive = true)
         {
-            throw new System.NotImplementedException();
+            return Entities.CountDocuments(filter => filter.IsActive == IsActive);
         }
 
         public string Create(T Item)
         {
-            throw new System.NotImplementedException();
+            if (!IdProvider.IsIdValid(Item.Id)) Item.Id = IdProvider.GenerateId();
+            Entities.InsertOne(Item);
+            return Item.Id;
         }
 
         public bool Delete(string Id)
         {
-            throw new System.NotImplementedException();
+            checkId(Id);
+            return Entities.DeleteOne(f => f.Id == Id).DeletedCount>0;
         }
 
         public bool Exists(string Id, bool IsActive = true)
         {
-            throw new System.NotImplementedException();
+            checkId(Id);
+            return Entities.CountDocuments(e => e.Id == Id && e.IsActive == IsActive) != 0;
         }
 
         public IEnumerable<T> Get(int Skip, int Take, bool IsActive = true)
         {
-            throw new System.NotImplementedException();
+            return Entities.Find(f => f.IsActive == IsActive).Skip(Skip).Limit(Take).ToEnumerable();
         }
 
         public T GetOne(string Id)
         {
-            throw new System.NotImplementedException();
+            checkId(Id);
+            return Entities.Find(f => f.Id == Id).FirstOrDefault();
         }
 
         public bool Update(T Item)
         {
-            throw new System.NotImplementedException();
+            if (Item == null) throw new ArgumentNullException(nameof(Item));
+            if (!Exists(Item.Id)) throw new ArgumentException("Объект еще не создан");
+            var update = createUpdateDefinition(GetOne(Item.Id), Item);
+            return Entities.FindOneAndUpdate(f => f.IsActive && f.Id == Item.Id, update)!=null;
         }
+
+        protected void checkId(string Id)
+        {
+            if (!IdProvider.IsIdValid(Id)) throw new ArgumentException("Некорректный идентификатор");
+        }
+        
     }
 }
