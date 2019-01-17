8c57bfe26e6db67209b65202af799229665fc468
Шиян Александр Евгеньевич (ДИТ) <Aleksandr.Shiyan@ingos.ru>
2019-01-17 17:17:42 +0300

    Optimization

--- API/Tenders.API/Services/EntrySaverService.cs
+++ API/Tenders.API/Services/EntrySaverService.cs
@@ -1,12 +1,14 @@
 ﻿using System;
 using System.Collections.Concurrent;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using Tenders.API.DAL.Interfaces;
 using Tenders.API.Enums;
 using Tenders.API.Models;
 using Tenders.API.Parameters;
 using Tenders.API.Services.Interfaces;
+using Tenders.Core.Abstractions.Services;
 
 namespace Tenders.API.Services
 {
@@ -15,22 +17,31 @@ namespace Tenders.API.Services
         private readonly IFTPEntryRepo _entryRepo;
         private readonly IFTPPathRepo _pathRepo;
         private readonly IIdProvider _idProvider;
+        private readonly ILoggerService _logger;
 
-        public EntrySaverService(IFTPEntryRepo entryRepo, IFTPPathRepo pathRepo, IIdProvider idProvider)
+        public EntrySaverService(IFTPEntryRepo entryRepo, IFTPPathRepo pathRepo, IIdProvider idProvider, ILoggerService logger)
         {
             _entryRepo = entryRepo ?? throw new ArgumentNullException(nameof(entryRepo));
             _pathRepo = pathRepo ?? throw new ArgumentNullException(nameof(pathRepo));
             _idProvider = idProvider ?? throw new ArgumentNullException(nameof(idProvider));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         }
 
         public void SaveFTPEntriesTree(string PathId, FTPEntriesTreeParam rootEntry)
         {
             var entriesToAdd = new ConcurrentBag<FTPEntry>();
+            var sw = new Stopwatch();
+            sw.Start();
             var entry = _entryRepo.ExistsByNameAndPathAndIsDirectory(rootEntry.Name, PathId, rootEntry.IsDirectory) ? _entryRepo.GetByNameAndPathAndIsDirectory(rootEntry.Name, PathId, rootEntry.IsDirectory) : null;
-            
             var res = _putEntry(entry, rootEntry, null, PathId, entriesToAdd);
+            _logger.Log($"Сохранил корень: {sw.Elapsed.Minutes}:{sw.Elapsed.Seconds}");
+            sw.Restart();
             _saveTree(res, rootEntry.Children, PathId, entriesToAdd);
+            _logger.Log($"Обошел дерево {sw.Elapsed.Minutes}:{sw.Elapsed.Seconds}");
+            sw.Restart();
             _entryRepo.CreateMany(entriesToAdd);
+            _logger.Log($"Записал новые файлы в базу {sw.Elapsed.Minutes}:{sw.Elapsed.Seconds}");
+            sw.Stop();
         }
 
         public int SaveRootEntriesWithoutChildren(string PathId, IEnumerable<FTPEntryParam> Entries)
@@ -103,20 +114,30 @@ namespace Tenders.API.Services
         }
 
         private void _saveTree(string parentId, IEnumerable<FTPEntriesTreeParam> inputChildren, string pathId, ConcurrentBag<FTPEntry> entriesToAdd)
-        {
-            
-            if (inputChildren == null) return;
+        {   
+            if (inputChildren == null || inputChildren.Count() == 0) return;
+            var sw = new Stopwatch();
+            sw.Start();
             Dictionary<string, FTPEntry> dbChildren = _entryRepo.GetByParentId(parentId).ToDictionary(dc => $"{dc.Name}_{dc.IsDirectory}");
-            inputChildren.AsParallel().ForAll(c =>
+            _logger.Log($"Получил все элементы по идентификатору родителя {sw.Elapsed.Minutes}:{sw.Elapsed.Seconds}");
+            sw.Restart();
+            var i = 0;
+            foreach(var c in inputChildren)
             {
                 var key = $"{c.Name}_{c.IsDirectory}";
                 var res = _putEntry(dbChildren.ContainsKey(key) ? dbChildren[key] : null, c, parentId, pathId, entriesToAdd);
+                if (c.Children == null || c.Children.Count() == 0) continue;
                 _saveTree(res, c.Children, pathId, entriesToAdd);
-            });
+                i++;
+            }
+            _logger.Log($"Цикл по детям {i} за {sw.Elapsed.Minutes}:{sw.Elapsed.Seconds}.{sw.Elapsed.Milliseconds}");
+            sw.Stop();
         }
 
         private string _putEntry(FTPEntry o, FTPEntriesTreeParam n, string parent, string pathId, ConcurrentBag<FTPEntry> entriesToAdd)
         {
+            var sw = new Stopwatch();
+            sw.Start();
             string res;
             if (o == null)
             {
@@ -147,8 +168,8 @@ namespace Tenders.API.Services
                 }
                 res = o.Id;
             }
-
-
+            _logger.Log($"Добавил элемент {sw.ElapsedMilliseconds}");
+            sw.Stop();
             return res ?? throw new InvalidOperationException($"Ошибка при сохранении пути {n.Name}. Идентификатор сохраненного объекта оказался null");
         }
         
